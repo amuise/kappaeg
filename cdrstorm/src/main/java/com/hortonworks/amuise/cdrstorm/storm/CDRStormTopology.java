d99c0217fbb3af8d555c7602e68f21d470086cfc
Adam Muise <amuise@hortonworks.com>
2014-09-25 13:28:43 -0400

    removed local Storm test cluster as we don't want to specify "compile" and bring on more classes than required into the uber jar

--- cdrstorm/src/main/java/com/hortonworks/amuise/cdrstorm/storm/CDRStormTopology.java
+++ cdrstorm/src/main/java/com/hortonworks/amuise/cdrstorm/storm/CDRStormTopology.java
@@ -5,52 +5,37 @@
  */
 package com.hortonworks.amuise.cdrstorm.storm;
 
-
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.StormSubmitter;
-import backtype.storm.task.OutputCollector;
-import backtype.storm.task.TopologyContext;
 import backtype.storm.testing.TestWordSpout;
-import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.TopologyBuilder;
-import backtype.storm.topology.base.BaseRichBolt;
-import backtype.storm.tuple.Fields;
-import backtype.storm.tuple.Tuple;
-import backtype.storm.tuple.Values;
 import backtype.storm.utils.Utils;
+import backtype.storm.topology.IRichSpout;
+
 /**
  *
  * @author adammuise
  */
 public class CDRStormTopology {
-    
-    public CDRStormTopology() {
-        
-    }
-    
-  public static void main(String[] args) throws Exception {
-    TopologyBuilder builder = new TopologyBuilder();
 
-    builder.setSpout("word", new TestWordSpout(), 10);
+    public CDRStormTopology() {
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        TopologyBuilder builder = new TopologyBuilder();
+
+        builder.setSpout("word", new TestWordSpout(), 10);
 //    builder.setBolt("exclaim1", new ExclamationBolt(), 3).shuffleGrouping("word");
 //    builder.setBolt("exclaim2", new ExclamationBolt(), 2).shuffleGrouping("exclaim1");
 
-    Config conf = new Config();
-    conf.setDebug(true);
+        Config conf = new Config();
+        conf.setDebug(true);
 
-    if (args != null && args.length > 0) {
-      conf.setNumWorkers(3);
+        conf.setNumWorkers(3);
 
-      //StormSubmitter.submitTopologyWithProgressBar(args[0], conf, builder.createTopology());
+        StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
+
     }
-    else {
-
-      LocalCluster cluster = new LocalCluster();
-      cluster.submitTopology("test", conf, builder.createTopology());
-      Utils.sleep(10000);
-      cluster.killTopology("test");
-      cluster.shutdown();
-    }
-  }
 }
